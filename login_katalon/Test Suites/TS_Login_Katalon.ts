992696cc079389e13db5776294308b4db1283a81
tund-hcmue <tund.fit@gmail.com>
2020-09-24 22:41:16 +0700

    change website

---
+++ login_katalon/Test Suites/TS_Login_Katalon.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login_Katalon</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>95437857-be42-4cdc-a856-ec1c2769a1b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef824e08-22a4-4a37-8387-df87d5996ca5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_null</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6cbd807d-2eb1-45a0-a053-5d05349c6c0a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02_userNull</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8c9930bb-1e75-4b71-a287-716c04293e86</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03_passNull</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aa9870eb-57cd-48f2-aa76-71c9cfaf9868</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04_succ</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4aec1639-fb80-40b0-b29b-f4a9a1e0a87b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC05_userPassFail</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
